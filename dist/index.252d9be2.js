5fe5cf65f50da10803f4d8c2dd82901a94224ac4
Carla <carlaillareguant@gmail.com>
2023-04-18 12:20:08 +0200

    First commit

---
+++ dist/index.252d9be2.js
@@ -0,0 +1,2 @@
+!function(){const e=document.getElementById("button-menu"),t=document.getElementById("nav");e.addEventListener("click",(()=>{e.classList.toggle("close"),t.classList.toggle("show")})),t.addEventListener("click",(s=>{"nav"===s.target.id&&(t.classList.remove("show"),e.classList.remove("close"))})),new IdealImageSlider.Slider({selector:"#slider",height:"auto",interval:4e3}).start()}();
+//# sourceMappingURL=index.252d9be2.js.map
---
+++ dist/index.70aa0100.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("button-menu"),t=document.getElementById("nav");e.addEventListener("click",(()=>{e.classList.toggle("close"),t.classList.toggle("show")})),t.addEventListener("click",(s=>{"nav"===s.target.id&&(t.classList.remove("show"),e.classList.remove("close"))})),new IdealImageSlider.Slider({selector:"#slider",height:"auto",interval:4e3}).start();
+//# sourceMappingURL=index.70aa0100.js.map
